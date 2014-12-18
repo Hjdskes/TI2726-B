59ec07a0525ad98289b7668f2e8be2c720347681
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2014-12-18 15:48:13 +0100

    updated gitignore, renamed messagetest to control

--- arduino/control/control.ino
+++ arduino/control/control.ino
@@ -1,20 +1,22 @@
-/*
- * rosserial Subscriber Example
- * Blinks an LED on callback
- */
-
 #include <ros.h>
 #include <std_msgs/String.h>
 
+class NewHardware : 
+public ArduinoHardware { 
+public: 
+  NewHardware() : 
+  ArduinoHardware(&Serial1, 57600) {
+  };
+};
+
+ros::NodeHandle_<NewHardware> nh;
+
 int FWD = 0;
 int REV = 1;
-int motor_left[] = {6, 7};
-int motor_right[] = {2, 3};
-
-class NewHardware : public ArduinoHardware { 
-  public: NewHardware() : ArduinoHardware(&Serial1, 57600) {};
-}; 
-ros::NodeHandle_<NewHardware> nh;
+int motor_left[] = {
+  6, 7};
+int motor_right[] = {
+  2, 3};
 
 void messageCb(const std_msgs::String& msg) {
   if (msg.data == "fwd") {
@@ -24,27 +26,25 @@ void messageCb(const std_msgs::String& msg) {
 
 ros::Subscriber<std_msgs::String> sub("direction", &messageCb);
 
-void setup()
-{
-   Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 
   // Setup motors
-  for(int i = 0; i < 2; i++){
+  for(int i = 0; i < 2; i++) {
     pinMode(motor_left[i], OUTPUT);
     pinMode(motor_right[i], OUTPUT);
   }
- 
+
   nh.initNode();
   nh.subscribe(sub);
 }
 
-void loop()
-{
+void loop() {
   nh.spinOnce();
   delay(500);
 }
 
-void motor_stop(){
+void motor_stop() {
   digitalWrite(motor_left[FWD], LOW);
   digitalWrite(motor_left[REV], LOW);
 
@@ -53,7 +53,7 @@ void motor_stop(){
   delay(25);
 }
 
-void drive_forward(){
+void drive_forward() {
   digitalWrite(motor_left[FWD], HIGH);
   digitalWrite(motor_left[REV], LOW);
 
@@ -61,7 +61,7 @@ void drive_forward(){
   digitalWrite(motor_right[REV], LOW);
 }
 
-void drive_backward(){
+void drive_backward() {
   digitalWrite(motor_left[FWD], LOW);
   digitalWrite(motor_left[REV], HIGH);
 
@@ -69,7 +69,7 @@ void drive_backward(){
   digitalWrite(motor_right[REV], HIGH);
 }
 
-void turn_left(){
+void turn_left() {
   digitalWrite(motor_left[FWD], LOW);
   digitalWrite(motor_left[REV], HIGH);
 
@@ -77,10 +77,12 @@ void turn_left(){
   digitalWrite(motor_right[REV], LOW);
 }
 
-void turn_right(){
+void turn_right() {
   digitalWrite(motor_left[FWD], HIGH);
   digitalWrite(motor_left[REV], LOW);
 
   digitalWrite(motor_right[FWD], LOW);
   digitalWrite(motor_right[REV], HIGH);
 }
+
+
