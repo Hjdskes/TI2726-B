595524002c099f5e220e55bb645968dd1ecb8647
Piet Agtmaal <piet@ubuntu.(none)>
2015-01-08 10:14:25 +0100

    libraries added, arduino/libraries removed from gitignore

---
+++ arduino/libraries/engine/engine.cpp
@@ -0,0 +1,63 @@
+/**
+ * Group number: 28
+ * Student 1:
+ * Jente Hidskes, 4335732
+ * Student 2:
+ * Piet van Agtmaal, 4321278
+ */
+
+#include "engine.h"
+#include "motor.h"
+
+Engine::Engine(Motor *left, Motor *right) : left(left), right(right) {
+}
+
+void Engine::turnLeft(int speed) {
+	left->backward(speed);
+	right->forward(speed);
+}
+
+void Engine::turnRight(int speed) {
+	left->forward(speed);
+	right->backward(speed);
+}
+
+void Engine::moveForward(int speed) {
+	left->forward(speed);
+	right->forward(speed);
+}
+
+void Engine::moveBackward(int speed) {
+	left->backward(speed);
+	right->backward(speed);
+}
+
+void Engine::move(bool forward, int speed, int angular) {
+	// FIXME: perhaps use angular to control speed when taking corners.
+	if (angular == 0) {
+		if (forward) {
+			moveForward(speed);
+		} else {
+			moveBackward(speed);
+		}
+	} else if (angular < 0) {
+		if (forward) {
+			turnLeft(speed);
+		} else {
+			turnRight(speed);
+		}
+	} else {
+		if (forward) {
+			turnRight(speed);
+		} else {
+			turnLeft(speed);
+		}
+	}
+}
+
+void Engine::stop() {
+	left->stop();
+	right->stop();
+	//delay(25); // FIXME: Original code had this. (Why) Was this needed?
+}
+
---
+++ arduino/libraries/motor/motor.h
@@ -0,0 +1,44 @@
+/**
+ * Group number: 28
+ * Student 1:
+ * Jente Hidskes, 4335732
+ * Student 2:
+ * Piet van Agtmaal, 4321278
+ */
+
+#ifndef _MOTOR_H_
+#define _MOTOR_H_
+
+/**
+ * This class represents one half of the LCHB-100 component.
+ *
+ * Speeds are taken as percentages and thus should be between 0 and 100
+ * inclusive.
+ */
+class Motor {
+	public:
+		/**
+		 * Constructs a new motor instance that will use the supplied pin layout.
+		 * It takes care of initialising the pinModes itself.
+		 */
+		Motor(int fwd, int rev, int en);
+		/**
+		 * Makes this motor go forward, using the given speed.
+		 */
+		void forward(int speed);
+		/**
+		 * Makes this motor go backward, using the given speed.
+		 */
+		void backward(int speed);
+		/**
+		 * Stops this motor.
+		 */
+		void stop();
+
+	private:
+		int fwd;
+		int rev;
+		int en;
+};
+
+#endif /* _MOTOR_H_ */
