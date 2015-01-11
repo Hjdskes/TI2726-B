6d963c6b7530df5dfc2b04017a15d0955fd875ae
Jente Hidskes <hjdskes@gmail.com>
2015-01-12 00:58:07 +0100

    Arduino: add stopping when no ROS message has been received.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -13,9 +13,9 @@
 #include "motor.h"
 #include "sensor.h"
 
-/* TODO: stop when no messages have been received for some time.
+/* TODO: test stopping when no messages have been received for one second.
    TODO: add a timer to the sketch to make the motor control interrupt based?
-   TODO: stop when the proximity sensor senses an object nearby.
+   TODO: test stopping when the proximity sensor senses an object nearby.
 */
 
 /* Overload the standard settings that are used
@@ -31,6 +31,8 @@ Sensor *sensor = NULL;
 ros::NodeHandle_<ArduinoBluetooth> nh;
 
 static void act(const geometry_msgs::Twist& twist) {
+	/* Reset counter upon processing ROS message. */
+	TCNT5 = 0;
 	engine->move(twist.linear.x > 0, twist.linear.x, twist.angular.z);
 }
 
@@ -38,6 +40,25 @@ void setup() {
 	const int LEFT_MOTOR[] = { 6, 7, 24 };
 	const int RIGHT_MOTOR[] = { 2, 3, 25 };
 	const int SENSOR[] = { 22, 23 };
+
+	/* Setup Timer5 to reliably fire a callback every second.
+	 *
+	 * To do so, our value in the compare match register must be:
+	 * (16000000 / (256 * 1)) - 1 = 62499, where 16000000 is the Arduino
+	 * that runs at 16MHz, 256 is our prescaler value (smaller is more accurate)
+	 * and 1Hz means once every second.
+	 *
+	 * See libraries/sensor/sensor.cpp for a list on which timers are available
+	 * and comments with all instructions. */
+	noInterrupts();
+	TCCR5A = 0;
+	TCCR5B = 0;
+	TCNT5 = 0;
+	OCR5A = 62499;
+	TCCR5B |= (1 << WGM12);
+	TCCR5B |= (1 << CS12);
+	TIMSK5 |= (1 << OCIE5A);
+	interrupts();
 
 	/* Setup engine. */
 	Motor left(LEFT_MOTOR[0], LEFT_MOTOR[1], LEFT_MOTOR[2]);
@@ -52,8 +73,20 @@ void setup() {
 	ros::Subscriber<geometry_msgs::Twist> sub = nh.subscribe("cmd_vel", 5, act);
 }
 
+ISR(TIMER5_COMPA_vect) {
+	/* If this interrupt launches it means that no ROS message has been received
+	 * in the passed second (because we reset the counter in the ROS callback),
+	 * so stop the engine. */
+	if (engine) {
+		engine->stop();
+	}
+}
+
 void loop() {
-	sensor.poll();
+	if (engine->isStopped()) {
+		engine->start();
+	}
+	sensor->poll();
 	nh.spinOnce();
 	delay(100);
 }
